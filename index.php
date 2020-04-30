0b66c25b840ee915722be398c4c9b2771988562b
Forz70043 <alfonso.pisicchio@libero.it>
2020-04-30 19:07:10 +0200

    cleaned index

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ include_once($_SERVER["DOCUMENT_ROOT"]."/common/inclusioni.php");
 
 sec_session_start();
 
-$smarty = new SmartyEngine();
 
 $smarty->display();
 
-error_log("XXX SESION ".print_r($_SESSION,1));
 ?>
