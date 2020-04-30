2e3f52b9317ef5021583dae864b550173cb3ea11
Forz70043 <alfonso.pisicchio@libero.it>
2020-04-30 19:06:20 +0200

    Improved autoload classes

--- common/inclusioni.php
+++ common/inclusioni.php
@@ -3,35 +3,27 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/config/general.config.php");
 require_once($_SERVER["DOCUMENT_ROOT"]."/functions/functions.php");
 require_once($_SERVER["DOCUMENT_ROOT"]."/config/db.config.php");
 include_once($_SERVER["DOCUMENT_ROOT"]."/includes/securimage/securimage.php");
-require($_SERVER["DOCUMENT_ROOT"].'/includes/Smarty/Smarty.class.php');
-require($_SERVER["DOCUMENT_ROOT"].'/includes/Smarty/SmartyEngine.class.php');
 
-/*
-$SEARCHDIRS=array(
-    //MODEL_PATH.'/'							=>	'.php',
-    DOCUMENT_ROOT.'/'.MODEL_PATH.'/'		=>	'.class.php',
-    //CONTROLLER_PATH.'/'							=>	'.php',
-    DOCUMENT_ROOT.'/'.CONTROLLER_PATH.'/'		=>	'.class.php',
-    //VIEW_PATH.'/'							=>	'.php',
-    DOCUMENT_ROOT.'/'.VIEW_PATH.'/'		=>	'.class.php'
-    
-);
-*/
-exec("find -name '*.class.php'",$classes,$ret);
-//error_log("ser docu root: ".print_r($_SERVER['DOCUMENT_ROOT'],1));
-/*foreach($classes as $class){
-    error_log("XXX path: ".$_SERVER['DOCUMENT_ROOT'] .$class);
-    include $_SERVER['DOCUMENT_ROOT'].$class;
-}
-*/
-//error_log('SEARCH '.$class_name);
-/*
-foreach($SEARCHDIRS as $path=>$suffix) {
-    error_log('TEST '.$path.$class_name.$suffix);
-    if(file_exists($path.$class_name.$suffix)) {
-        include($path.$class_name.$suffix);
-        return;
+function __autoload($classname){
+    $searchDirs=array(
+        DOCUMENT_ROOT.'/'.'mvc/model/'       =>'.class.php',
+        DOCUMENT_ROOT.'/'.'mvc/controller/'  =>'.class.php',
+        DOCUMENT_ROOT.'/'.'mvc/model/form/'=>'.class.php',
+        DOCUMENT_ROOT.'/'.'includes/Smarty/'=>'.class.php'
+    );
+    foreach ($searchDirs as $path => $suffix) {
+        if(file_exists($path.$classname.$suffix)){
+            error_log("XXX file: ".print_r($path.$classname.$suffix,1));
+            include($path.$classname.$suffix);
+            return;
+        }
     }
 }
-*/
+
+spl_autoload_register("__autoload");
+global $smarty; 
+
+$smarty = new SmartyEngine();
+
+
 ?>
