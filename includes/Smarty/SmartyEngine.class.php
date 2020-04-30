1c1fdf336f7263da6be4641de94b474c22b171db
Forz70043 <alfonso.pisicchio@libero.it>
2020-04-30 19:09:02 +0200

    Improved Smarty display

--- includes/Smarty/SmartyEngine.class.php
+++ includes/Smarty/SmartyEngine.class.php
@@ -8,21 +8,30 @@ class SmartyEngine extends Smarty
 	{
 		parent::__construct();
 
-		$this->setTemplateDir($_SERVER["DOCUMENT_ROOT"].'/views');
+		$this->setTemplateDir(SMARTY_TPL_DIR);
 		$this->setCompileDir($_SERVER["DOCUMENT_ROOT"].'/includes/Smarty/templates_c');
 		$this->setCacheDir($_SERVER["DOCUMENT_ROOT"].'/includes/Smarty/cache');
 		$this->setConfigDir($_SERVER["DOCUMENT_ROOT"].'/includes/Smarty/configs');
 
-		$this->left_delimiter='[+';
-		$this->right_delimiter='+]';
+		$this->left_delimiter=SMARTY_L_DELIMITER;
+		$this->right_delimiter=SMARTY_R_DELIMITER;
 		$this->assign('app_name', 'WILDCLOUD');
 		$this->assign('doc_root',$_SERVER['DOCUMENT_ROOT']);
+		$this->assign('home','/');
 	}
 
-	public function display($tpl_component=null){
-		if(isset($tpl_component)) $this->assign('bodyComponents',$tpl_component);
-
-		return parent::display('index.tpl');
+	public function display($tplname=null){
+		if(isset($tplname)) $this->assign('view',$tplname);
+		$tpl='index.tpl';
+		return parent::display($tpl);
+	}
+	
+	public function displayForm($form,$formTpl)
+	{
+		$this->assign('form',$form);
+		$this->assign('formTpl','forms/'.$formTpl);
+		
+		$this->display('forms/base.tpl');
 	}
 };
 
