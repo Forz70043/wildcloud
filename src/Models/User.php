1f776583fe1c729d4e0328d6cfbf50a12b8b30f1
Forz70043 <alfonso.pisicchio@libero.it>
2026-02-28 09:13:46 +0100

    Update Db class & user model

--- src/Models/User.php
+++ src/Models/User.php
@@ -4,5 +4,25 @@ namespace WildCloud\Models;
 
 class User
 {
+    /**
+     * @param int $id
+     * @param string $username
+     * @param string $email
+     * @param string $passwordHash
+     */
+    public function __construct(
+        public readonly int $id,
+        public readonly string $username,
+        public readonly string $email,
+        private readonly string $passwordHash
+    ) {}
 
+    /**
+     * @param string $password
+     * @return bool
+     */
+    public function verifyPassword(string $password): bool
+    {
+        return password_verify($password, $this->passwordHash);
+    }
 }
--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -1,8 +1,39 @@
 <?php
 
-namespace WildCloud\Sevices;
+namespace WildCloud\Services;
+
+use WildCloud\Core\Database;
+use WildCloud\Models\User;
 
 class AuthService
 {
+    /**
+     * @param Database $db
+     */
+    public function __construct(private readonly Database $db) {}
 
+    /**
+     * @param string $username
+     * @param string $password
+     * @return User|null
+     */
+    public function login(string $username, string $password): ?User
+    {
+        $pdo = $this->db->connect();
+
+        $stmt = $pdo->prepare("SELECT id, username, email, password FROM users WHERE username = :user");
+        $stmt->execute(['user' => $username]);
+        $data = $stmt->fetch();
+
+        if ($data && password_verify($password, $data['password'])) {
+            return new User(
+                $data['id'],
+                $data['username'],
+                $data['email'],
+                $data['password']
+            );
+        }
+
+        return null;
+    }
 }
